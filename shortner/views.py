f85d280cea4d9830704efdc922dcbbd96c8f7128
Ninaad <ninaadravi@icloud.com>
2021-09-11 19:55:38 +0530

    finished website

---
+++ shortner/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link,uuid=uid)
+        new_url.save()
+        return render(request, 'finished.html', {'final': "http://127.0.0.1:8000/"+uid})
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
